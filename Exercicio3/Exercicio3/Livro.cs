1558a014ac68c80eb55eef58217f041433433696
KillerCookie <71276068+KillerCookie@users.noreply.github.com>
2021-03-30 15:11:08 -0300

    Exercicio feito

---
+++ Exercicio3/Exercicio3/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio3
+{
+    class Livro : Produto
+    {
+        public string Autor { get; set; }
+
+        public Livro(string titulo, double preco, string autor) : base(titulo, preco)
+        {
+            Autor = autor;
+        }
+
+        public override void InformarDescricao()
+        {
+            Console.WriteLine($"{Titulo} escrito por {Autor}");
+        }
+
+
+
+    }
+}
---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto Yakusa = new JogoDigital("Yakusa0", 36, "PS4");
+            Produto Hades = new JogoDigital("Hades", 37, "PS4");
+            Produto Livro1 = new Livro("Metamorfose", 40, "Franz Kafka");
+            Produto Livro2 = new Livro("O Iluminado", 40, "Stephen King");
+
+            Produto[] produtos = { Yakusa, Hades, Livro1, Livro2 };
+
+            foreach (Produto item in produtos)
+            {
+                item.InformarDescricao();
+                item.InformarPreco();
+                Console.WriteLine("...........");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
